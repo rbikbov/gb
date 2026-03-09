1d96a74328eb7cf8f5ff7a0f4f3066289ef3f606
Rafis Bikbov <dribblerafis@gmail.com>
2026-03-09 16:41:56 +0000

    feat: add tailwind4 for cleaner styles

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-import './Counter.css'
 
 export function Counter() {
   const [count, setCount] = useState(0)
 
   return (
-    <button className="counter-button" onClick={() => setCount((count) => count + 1)}>
+    <button 
+      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors cursor-pointer"
+      onClick={() => setCount((count) => count + 1)}
+    >
       count is {count}
     </button>
   )
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,30 +1,18 @@
-import reactLogo from '@/shared/assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Counter } from '@/features/counter'
-import './MainPage.css'
 
 function MainPage() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container min-h-screen mx-auto py-4">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-8">Users list</h1>
+        <div className="rounded-lg p-8">
+          <Counter />
+          <p className="mt-4 text-sm text-gray-600">
+            Edit <code className="px-1 rounded">src/pages/main/MainPage.tsx</code> and save to test HMR
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Counter />
-        <p>
-          Edit <code>src/pages/main/MainPage.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
